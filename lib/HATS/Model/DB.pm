fd85d6d6b21595916a0557d55a3d9d87f407a564
newbcode <newbcode@debian>
2013-03-21 16:41:44 +0900

    DB Password Change

--- lib/HATS/Model/DB.pm
+++ lib/HATS/Model/DB.pm
@@ -9,7 +9,7 @@ __PACKAGE__->config(
     connect_info => {
         dsn => 'dbi:mysql:database=hats',
         user => 'hats',
-        password => 'sado999',
+        password => 'qwe123',
     }
 );
 
