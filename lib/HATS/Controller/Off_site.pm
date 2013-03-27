7c2ae9237af0779ce4a7f7fdc7017f34f3a56dc4
newbcode <newbcode@newb-dtop>
2013-03-28 00:13:09 +0900

    off_site page add

---
+++ lib/HATS/Controller/Off_site.pm
@@ -0,0 +1,45 @@
+package HATS::Controller::Off_site;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+HATS::Controller::Off_site - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    #$c->response->body('Matched HATS::Controller::Off_site in Off_site.');
+    $c->stash(template => 'off_site/main.tt');
+}
+
+
+=head1 AUTHOR
+
+newbcode,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
